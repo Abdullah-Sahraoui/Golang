0ce2bc899a6177da533c7bc604f430f5b8546c53
Abd S <abd.sahraoui26@gmail.com>
2021-01-15 15:01:07 +0000

    Added some testing for our functions.

--- Card-Game/Cards/deck.go
+++ Card-Game/Cards/deck.go
@@ -16,8 +16,8 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
-	cardValues := []string{"Ace", "One", "Two", "Three", "Four"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
--- Card-Game/Cards/deck_test.go
+++ Card-Game/Cards/deck_test.go
@@ -1,11 +1,40 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
+	// To ensure length of deck is 16
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	// To see if first and last cards are ace of spades and four of clubs respectively
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	// To ensure saveToFile() and newDeckFromFile() work properly
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
