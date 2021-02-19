7a140f43977ebb5887993f0da302b92741825209
Abdullah-Sahraoui <abd.sahraoui26@gmail.com>
2021-02-19 22:20:36 +0000

    Cleaner way of completing task.
    Used strconv package Unquote method.

--- AOC 2015/Day 8/day8.go
+++ AOC 2015/Day 8/day8.go
@@ -3,52 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	bs, err := ioutil.ReadFile("./input.txt")
+	input, err := ioutil.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
 	}
-	file := strings.Split(string(bs), "\r\n")
 
-	c1, c2 := counting(file)
-	fmt.Println("c1 - c2: ", c1-c2)
-}
+	file := strings.Split(string(input), "\n")
 
-func reg1(s string) int {
-	re := regexp.MustCompile(`(\\x)[0-9a-z][0-9a-z]`)
-	m := re.FindAllString(s, -1)
-	return len(m)
-}
+	var cc int
+	var mc int
 
-func reg2(s string) int {
-	re := regexp.MustCompile(`\\"`)
-	m := re.FindAllString(s, -1)
-	return len(m)
-}
-
-func reg3(s string) int {
-	re := regexp.MustCompile(`\\\\`)
-	m := re.FindAllString(s, -1)
-	return len(m)
-}
-
-func counting(s []string) (int, int) {
-	c1 := 0
-	c2 := 0
-	for _, str := range s {
-		c1 += len(str)
-		c2 += len(str) - 2
-
-		r1 := (reg1(str)) * 3
-		r2 := reg2(str)
-		r3 := reg3(str)
-
-		c2 -= (r1 + r2 + r3)
+	for _, l := range file {
+		cc += len(l)
+		unquoted, err := strconv.Unquote(l)
+		if err != nil {
+			panic(err)
+		}
+		mc += len(unquoted)
 	}
-
-	return c1, c2
+	fmt.Println(cc - mc)
 }
